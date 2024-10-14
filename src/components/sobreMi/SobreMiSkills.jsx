e82d7d042776feae3c6b2d2ae737d5d6ea0e58ea
¨Edgar <s.steinberg2019@gmail.com>
2024-10-14 20:20:22 -0300

    update

--- src/components/sobreMi/SobreMiSkills.jsx
+++ src/components/sobreMi/SobreMiSkills.jsx
@@ -10,6 +10,7 @@ const SobreMiSkills = ({ skills }) => {
                     {skills.frontend && skills.frontend.map((skill, index) => (
                         <div className={styles.divContainerTools} key={index} >
                             <img className={styles.img_skills} src={`https://porfolio-back-lr6x.onrender.com/skills/frontend/${skill.image}`} alt={`skill ${index}`} />  
+                            <p className={styles.p}>{skill.title}</p>
                         </div>
                     ))}
                 </div>
@@ -20,6 +21,7 @@ const SobreMiSkills = ({ skills }) => {
                     {skills.backend && skills.backend.map((skill, index) => (
                         <div className={styles.divContainerTools} key={index} >
                             <img className={styles.img_skills} src={`https://porfolio-back-lr6x.onrender.com/skills/backend/${skill.image}`} alt={`skill ${index}`} /> 
+                            <p className={styles.p}>{skill.title}</p>
                         </div>
                     ))}
                 </div>
