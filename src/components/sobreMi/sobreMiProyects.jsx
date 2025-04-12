6df4b1c10b8b251eb4e385a3c659c407a56b462a
¨Edgar <s.steinberg2019@gmail.com>
2025-04-12 18:54:27 -0300

    add proyect

--- src/components/sobreMi/sobreMiProyects.jsx
+++ src/components/sobreMi/sobreMiProyects.jsx
@@ -7,10 +7,27 @@ const SobreMiProyects = () => {
         <>
             <div className={styles.proyectContainer}>
                 <div className={styles.proyect}>
+                    <img className={styles.img_proyects} src='/cartFullStack.jpg' alt='proyecto' />
+                    <p>
+                    Aplicación de e-commerce desarrollada con React para el frontend y Node.js con Express en el backend. Permite a los usuarios registrarse, iniciar sesión y completar su perfil con datos personales para realizar compras. El sistema genera un ticket al finalizar la transacción, y gestiona el acceso mediante roles: usuarios, usuarios premium y administradores. Los usuarios pueden subir documentos para solicitar el rol premium, y se envían notificaciones por correo en caso de eliminación de productos. También incluye un sistema de recuperación de contraseña por email, asegurando una experiencia segura y completa. Toda la gestión de productos y usuarios está optimizada para facilitar la administración desde el panel correspondiente.
+                    </p>
+                    <Flex
+                        horizontal="true"
+                        gap="small"
+                        style={{ width: '100%', display: 'flex', justifyContent: 'center', marginTop: '5px' }}>
+                        <a href="https://github.com/EdgarSteinberg/full_stack_react" target={'_blank'} rel="noopener noreferrer">
+                            <Button type='primary'><GithubOutlined />GitHub</Button>
+                        </a>
+                        <a href="https://mobilematrix.netlify.app/" target={'_blank'} rel="noopener noreferrer">
+                            <Button type='primary'><RightSquareOutlined />ver proyecto</Button>
+                        </a>
+                    </Flex>
+                </div>
+                <div className={styles.proyect}>
                     <h3>E-commerce Fullstack</h3>
                     <img className={styles.img_proyects} src='/caputra_proyecto_backend.jpg' alt='proyecto1' />
                     <p>
-                        Este proyecto es una plataforma de comercio electrónico especializada en la venta de guitarras, construida con una arquitectura MVC. Permite a los usuarios registrarse, iniciar sesión y realizar compras.
+                        Este proyecto es una plataforma de comercio electrónico desarrollada con arquitectura MVC. Permite a los usuarios registrarse, iniciar sesión y realizar compras.
                         Está dividido en tres roles: <strong>usuario</strong>, <strong>usuario premium</strong>, y <strong>administrador</strong>, quienes tienen diferentes niveles de permisos. Los usuarios premium pueden crear y eliminar productos, mientras que los administradores también pueden gestionar usuarios y eliminar productos.
                         El sistema almacena la última conexión de los usuarios, y si permanecen inactivos durante mucho tiempo, el administrador puede eliminarlos, enviándoles una notificación por correo electrónico. Además, los usuarios premium son notificados cuando alguno de sus productos es eliminado.
                         El proyecto incluye una sección de contacto, y utiliza <strong>WebSockets</strong> para una experiencia en tiempo real.
